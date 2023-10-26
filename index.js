b8c55abd29c43a7f7f1ae56915247b9192eeacf8
Its-Yayo <elyayoveloz@gmail.com>
2023-10-26 17:00:25 -0600

    Update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const sequelize = require('./util/database')
 //Middleware 
 app.use(express.json());
 
-//Mala practica
+//Muy mala practica
 app.get('/test', (req, res) => {
     console.log("Esto no se debe de hacer, pero funciona.")
     res.send('<h1> Servidor funcionando </h1>');
